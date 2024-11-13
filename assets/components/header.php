656a421850a922b227a389b49b6125ee746ebe91
MichiNowa <michinowa13@gmail.com>
2024-11-13 21:42:30 +0800

    m

--- assets/components/header.php
+++ assets/components/header.php
@@ -9,7 +9,7 @@
     <!-- Logo Icon -->
     <link rel="icon" href="<?= pathname('images/logo.jpg') ?>">
     <!-- fonts -->
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
     <!-- Tailwind CSS (Prefix: tw-) -->
     <link rel="stylesheet" href="<?= pathname('css/tailwind.css') ?>">
     <!-- Bootstrap CSS -->
@@ -17,22 +17,36 @@
     <!-- Bootstrap Icons -->
     <link href="<?= pathname('vendor/bootstrap/icons/bootstrap-icons.css') ?>" rel="stylesheet">
     <!-- Sweetalert2 CSS -->
-    <link href="<?= pathname('vendor/sweetalert2/sweetalert2.min.css')?>" rel="stylesheet">
+    <link href="<?= pathname('vendor/sweetalert2/sweetalert2.min.css') ?>" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="<?= pathname('css/custom.css') ?>" rel="stylesheet">
 
     <!-- Page Title -->
     <title><?= $page_title ?></title>
+    <script type="importmap">
+        {
+            "imports": {
+                "react": "https://esm.sh/react@18.3.1",
+                "react-qr-scanner": "https://esm.sh/@yudiel/react-qr-scanner@2.0.8",
+                "clsx": "https://esm.sh/clsx@2.1.1",
+                "react-dom": "https://esm.sh/react-dom@18.3.1/client",
+                "react-dom/server": "https://esm.sh/react-dom@18.3.1/server",
+                "sweetalert2": "https://esm.sh/sweetalert2@11.12.4",
+                "react-pdf": "https://esm.sh/react-pdf@9.1.0"
+            }
+        }
+    </script>
     <!-- Sweetalert2 JS -->
     <script src="<?= pathname('vendor/sweetalert2/sweetalert2.min.js') ?>"></script>
 </head>
 
 <body>
-<?php
-if (!isset($_SESSION['backed'])) {
-?>
-<div id="loading-spinner">
-    <img src="<?= pathname("images/guidance-logo-loader.png"); ?>" alt="logo" class="tw-w-[150px] tw-h-[150px] tw-object-contain tw-animate-pulse tw-bg-transparent tw-p-4" />
-</div>
-<div id="content-body">
-<?php } ?>
+    <?php
+    if (!isset($_SESSION['backed'])) {
+        ?>
+        <div id="loading-spinner">
+            <img src="<?= pathname("images/guidance-logo-loader.png"); ?>" alt="logo"
+                class="tw-w-[150px] tw-h-[150px] tw-object-contain tw-animate-pulse tw-bg-transparent tw-p-4" />
+        </div>
+        <div id="content-body">
+        <?php } ?>
--- assets/components/header_with_navbar.php
+++ assets/components/header_with_navbar.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,7 +15,7 @@
     <!-- Bootstrap Icons -->
     <link href="<?= pathname('vendor/bootstrap/icons/bootstrap-icons.css') ?>" rel="stylesheet">
     <!-- Sweetalert2 CSS -->
-    <link href="<?= pathname('vendor/sweetalert2/sweetalert2.min.css')?>" rel="stylesheet">
+    <link href="<?= pathname('vendor/sweetalert2/sweetalert2.min.css') ?>" rel="stylesheet">
 
     <!--========== CDN ==========-->
     <link rel="stylesheet" href="<?= pathname('vendor/boxicons/boxicons.min.css') ?>">
@@ -34,40 +35,55 @@
             cursor: pointer;
         }
     </style>
+    <script type="importmap">
+        {
+            "imports": {
+                "react": "https://esm.sh/react@18.3.1",
+                "react-qr-scanner": "https://esm.sh/@yudiel/react-qr-scanner@2.0.8",
+                "clsx": "https://esm.sh/clsx@2.1.1",
+                "react-dom": "https://esm.sh/react-dom@18.3.1/client",
+                "react-dom/server": "https://esm.sh/react-dom@18.3.1/server",
+                "sweetalert2": "https://esm.sh/sweetalert2@11.12.4",
+                "react-pdf": "https://esm.sh/react-pdf@9.1.0"
+            }
+        }
+    </script>
     <!-- Sweetalert2 JS -->
     <script src="<?= pathname('vendor/sweetalert2/sweetalert2.min.js') ?>"></script>
 
 </head>
 
 <body>
-<?php
-if (!isset($_SESSION['backed'])) {
-?>
-<div id="loading-spinner">
-<img src="<?= pathname("images/guidance-logo-loader.png"); ?>" alt="logo" class="tw-w-[150px] tw-h-[150px] tw-object-contain tw-animate-pulse tw-bg-transparent tw-p-4" />
-</div>
-<div id="content-body">
-<?php } ?>
-
-<!--========== HEADER ==========-->
-<header class="header">
-    <div class="header__container">
-        <?php if ($user) { ?>
-            <img src="<?= pathname(!$user->profile_pic ? "images/default-user.png" : $user->profile_pic) ?>" alt="" class="header__img">
-        <?php } else { ?>
-            <a href="<?= pathname('login') ?>" class="header__link">Login</a>
+    <?php
+    if (!isset($_SESSION['backed'])) {
+        ?>
+        <div id="loading-spinner">
+            <img src="<?= pathname("images/guidance-logo-loader.png"); ?>" alt="logo"
+                class="tw-w-[150px] tw-h-[150px] tw-object-contain tw-animate-pulse tw-bg-transparent tw-p-4" />
+        </div>
+        <div id="content-body">
         <?php } ?>
-        <span href="#" class="header__logo">SMCC Guidance Center</span>
 
-        <!-- <div class="header__search">
+        <!--========== HEADER ==========-->
+        <header class="header">
+            <div class="header__container">
+                <?php if ($user) { ?>
+                    <img src="<?= pathname(!$user->profile_pic ? "images/default-user.png" : $user->profile_pic) ?>" alt=""
+                        class="header__img">
+                <?php } else { ?>
+                    <a href="<?= pathname('login') ?>" class="header__link">Login</a>
+                <?php } ?>
+                <span href="#" class="header__logo">SMCC Guidance Center</span>
+
+                <!-- <div class="header__search">
             <input type="search" placeholder="Search" class="header__input">
             <i class='bx bx-search header__icon'></i>
         </div> -->
 
-        <div class="header__toggle">
-        <i class='bx bx-menu' id="header-toggle"></i>
-        </div>
-    </div>
-</header>
+                <div class="header__toggle">
+                    <i class='bx bx-menu' id="header-toggle"></i>
+                </div>
+            </div>
+        </header>
 
-<?= $sidebar ?>
+        <?= $sidebar ?>
